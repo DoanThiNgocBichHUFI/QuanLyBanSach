ee5b95f06c1881952947ec884882ea5e6caabaa8
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2023-10-29 11:50:17 +0700

    Create ChuDeController.cs
    Tạo ChuDeController, trong đó có action
    - ChuDePartial() : trả về danh sách tên các chủ đề trong đối tượng ChuDes (Lấy 7 chủ đề đầu tiên và sắp xếp theo tên chủ đề)

---
+++ ChuDeController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai1.Models;
+
+namespace bai1.Controllers
+{
+    public class ChuDeController : Controller
+    {
+        //
+        // GET: /ChuDe/
+        dbQuanLyBanSachDataContext db = new dbQuanLyBanSachDataContext();
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = db.ChuDes.Take(7).OrderBy(cd => cd.TenChuDe).ToList();
+            return View(listChuDe);
+        }
+	}
+}
