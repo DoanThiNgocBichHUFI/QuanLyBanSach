5ab97f6c3cdb93b40587f43bef9f56221525ba52
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2023-10-29 12:42:57 +0700

    Create SachController.cs
    tạo action SachTheoChuDe lấy 7 chủ đề đầu tiên và sắp xếp theo tên chủ đề

---
+++ SachController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai1.Models;
+
+namespace bai1.Controllers
+{
+    public class SachController : Controller
+    {
+        QLBanSachDataContext db = new QLBanSachDataContext();
+        //
+        // GET: /Sach/
+        public ActionResult AllSach()
+        {
+            return View(db.Saches.ToList());
+        }
+        public ActionResult SachTheoChuDe(int MaCD)
+        {
+            var listSach = db.Saches.Where(s => s.MaChuDe == MaCD).OrderBy(s => s.GiaBan).ToList();
+            if(listSach.Count == 0)
+            {
+                ViewBag.TB = "Ko co sach thuoc chu de nay";
+            }
+            return View(listSach);
+        }
+
+	}
+}
