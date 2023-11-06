37c9c92bbb833b20426c4ae11c6ae132164c3101
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2023-11-06 09:59:52 +0700

    Create GioHangController.cs
    Xây dựng phương thưc lấy giỏ hàng

---
+++ GioHangController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai1.Models;
+
+namespace bai1.Controllers
+{
+    public class GioHangController : Controller
+    {
+        dbQuanLyBanSachDataContext db = new dbQuanLyBanSachDataContext();
+        //
+        // GET: /GioHang/
+        
+        public List<GioHang> layGioHang()
+        {
+            List<GioHang> lstGioHang = Session["GioHang"] as List<GioHang>;
+            if (lstGioHang == null)
+            {
+                lstGioHang = new List<GioHang>();
+                Session["GioHang"] = lstGioHang;
+            }
+            return lstGioHang;
+        }
+        
+	}
+}
