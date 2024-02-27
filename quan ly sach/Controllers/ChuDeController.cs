f201a51e4a1088ce5bcae6bc95d5244453b333b6
DoanThiNgocBichHUFI <97453724+DoanThiNgocBichHUFI@users.noreply.github.com>
2024-02-27 16:46:15 +0700

    Add files via upload

---
+++ quan ly sach/Controllers/ChuDeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bai1.Models;
+
+namespace bai1.Controllers
+{
+    public class ChuDeController : Controller
+    {
+        QLBanSachDataContext db = new QLBanSachDataContext();   
+        //
+        // GET: /ChuDe/
+        public ActionResult ChuDePartial()
+        {
+            var listCD = db.ChuDes.Take(7).ToList();
+            return View(listCD);
+        }
+        public ActionResult NXBPartial()
+        {
+            var listNXB = db.NhaXuatBans.Take(7).ToList();
+            return View(listNXB);
+        }
+	}
+}
